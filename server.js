b0b6b4c3b2f2c0cb0b3d1c083230d06e7edb2c10
DIGVIJAY <singhdigvijay703@gmail.com>
2023-04-08 01:22:18 +0530

    .

--- server.js
+++ server.js
@@ -6,12 +6,17 @@ const userRouter = require("./routes/userRouter");
 const app = express();
 const PORT = 8080;
 
+app.use(express.json());
+
 connectMongoDB("mongodb://localhost:27017/crud-test").then(() => {
   console.log("DB Connected!!!");
+}).catch((err) => {
+  console.log(`DB Connection Error: ${err.message}`);
+  process.exit(1);
 });
 
-app.use("/api/user", userRouter);
+app.use("/api/users", userRouter);
 
 app.listen(PORT, () => {
-  console.log(`Server started at PORT:${PORT}`);
+  console.log(`Server started at http://localhost:${PORT}/`);
 });
