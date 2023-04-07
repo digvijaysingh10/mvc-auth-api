d923ea490234fa8bd730323f36f4b4ee2ba3c7c9
DIGVIJAY <singhdigvijay703@gmail.com>
2023-04-08 00:56:23 +0530

    .

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,44 +1,83 @@
 const User = require("../models/userModel");
 
-async function handleGetAllUsers(req, res) {
-  const allUsers = await User.find({});
-  return res.json(allUsers);
-}
-
-async function handleGetUserById(req, res) {
-  const user = await User.findById(req.params.id);
-  if (!user) return res.status(404).json({ error: "USER NOT FOUND!" });
-  return res.json(user);
-}
-async function handleUpdateUserById(req, res) {
-  await User.findByIdAndUpdate(req.params.id);
-  return res.json({ status: "SUCESS" });
-}
-
-async function handleDeleteUserById(req, res) {
-  await User.findByIdAndDelete(req.params.id);
-  return res.json({ status: "SUCESS" });
-}
-
-async function handleCreateNewUser(req, res) {
-  const body = req.body;
-  if (!body || !body.firstname || !body.lastname || !body.email || !body.password) {
-    return res.status(400).json({
-      message: "All fields are required",
-    });
+const handleGetAllUsers = async (req, res) => {
+  try {
+    const allUsers = await User.find();
+    if (!allUsers) {
+      return res.status(404).json({ message: "USER NOT FOUND!!!" });
+    }
+    return res.json({ allUsers });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({ error: "Server error" });
   }
-  const result = await User.create({
-    firstname: body.firstname,
-    lastname: body.lastname,
-    email: body.email,
-    password: body.password,
-  });
-  return res.status(201).json({
-    message: "User created successfully",
-    id: result._id,
-  });
-}
+};
 
+const handleGetUserById = async (req, res) => {
+  try {
+    const user = await User.findById(req.params.id);
+    if (!user) {
+      return res.status(404).json({ error: "USER NOT FOUND!!!" });
+    }
+    return res.json({ user });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({ error: "Server error" });
+  }
+};
+
+const handleUpdateUserById = async (req, res) => {
+  try {
+    await User.findByIdAndUpdate(req.params.id, req.body);
+    return res.json({ status: "SUCCESS" });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({ error: "Server error" });
+  }
+};
+
+const handleDeleteUserById = async (req, res) => {
+  try {
+    const deletedUser = await User.findByIdAndDelete(req.params.id);
+    if (!deletedUser) {
+      return res.status(404).json({ error: "USER NOT FOUND!!!" });
+    }
+    return res.json({ status: "SUCCESS" });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({ error: "Server error" });
+  }
+};
+
+const handleCreateNewUser = async (req, res) => {
+  try {
+    const body = req.body;
+    if (
+      !body ||
+      !body.firstname ||
+      !body.lastname ||
+      !body.email ||
+      !body.password
+    ) {
+      return res.status(400).json({
+        message: "All fields are required",
+      });
+    }
+    const result = await User.create({
+      firstname: body.firstname,
+      lastname: body.lastname,
+      email: body.email,
+      password: body.password,
+    });
+    return res.status(201).json({
+      message: "User created successfully",
+      id: result._id,
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({ error: "Server error" });
+  }
+};
 
 module.exports = {
   handleGetAllUsers,
