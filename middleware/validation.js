b69635f4dde5619f49217b021a0e5a121a7187ea
VALIANTGUARDIAN <singhdigvijay703@gmail.com>
2023-04-08 13:43:33 +0530

    validation.js file added

---
+++ middleware/validation.js
@@ -0,0 +1,19 @@
+const joi = require("@hapi/joi");
+
+const registerValidation = data => {
+    const schema = {
+        firstname: joi.string().min(2).required(),
+        lastname: joi.string().min(2).required(),
+        email: joi.string().email().required(),
+        password: joi.string().min(8).required()
+    };
+    return joi.ValidationError(data, schema);
+};
+const loginValidation = data => {
+    const schema = {
+        email: joi.string().email().required(),
+        password: joi.string().min(8).required()
+    };
+    return joi.ValidationError(data, schema);
+};
+module.exports = { registerValidation, loginValidation }
