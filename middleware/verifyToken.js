7f055a97cb30645cc1c8f11a51f9160cab5235f4
VALIANTGUARDIAN <singhdigvijay703@gmail.com>
2023-04-08 13:08:10 +0530

    jwt verification

---
+++ middleware/verifyToken.js
@@ -0,0 +1,17 @@
+const jwt = require("jsonwebtoken");
+
+const auth = (req, res, next) => {
+    const token = req.header('auth-token');
+    if (!token) {
+        return res.status(401).send("ACCESS DENIED!!!");
+    }
+    try {
+        const verified = jwt.verify(token, process.env.TOKEN_SECRET);
+        req.user = verified;
+        next();
+    } catch (error) {
+        res.status(400).send("INVALID TOKEN!!!");
+    }
+}
+
+module.exports = { auth }
