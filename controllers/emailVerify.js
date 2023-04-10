e1bbddd5e84aea1402e64abc96336ab0352edfd3
VALIANTGUARDIAN <singhdigvijay703@gmail.com>
2023-04-10 10:21:34 +0530

    mail controller

---
+++ controllers/emailVerify.js
@@ -0,0 +1,24 @@
+const sendMail = async(email, subject, text) =>{
+    try{
+        const transporter = nodemialer.createTransporrt({
+            host:smtp.gmail.com,
+            service:gmail,
+            port: Number(process.env.EMAIL_PORT),
+            secure: true,
+            auth:{
+                user:'digvijay.singh@indicchain.com',
+                password:'TestPass@123'
+            }
+        });
+        await transporter.sendMail({
+            from:'digvijay.singh@indicchain.com',
+            to:email,
+            subject:subject,
+            text:text
+        });
+        console.log("EMAIL SENT!");
+    }catch(error){
+        console.log("EMAIL NOT SENT!");
+    }
+    console.log(error);
+}
