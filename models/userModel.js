a78b9b06f2610ea552f1ad2adf8493a19681d2d2
VALIANTGUARDIAN <singhdigvijay703@gmail.com>
2023-04-10 10:23:09 +0530

    userModel change

--- models/userModel.js
+++ models/userModel.js
@@ -16,6 +16,10 @@ const userSchema = new mongoose.Schema({
   password: {
     type: String,
     required: true,
+  },
+  verified: {
+    type: Boolean,
+    default: false
   }
 }, { timestamps: true });
 
