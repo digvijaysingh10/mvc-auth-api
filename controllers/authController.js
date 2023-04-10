269b3e0d3ea39417f6d4f6b6de5050ece36ed64a
VALIANTGUARDIAN <singhdigvijay703@gmail.com>
2023-04-10 10:54:49 +0530

    verify signup completed

--- controllers/authController.js
+++ controllers/authController.js
@@ -21,8 +21,13 @@ const handleSignup = async (req, res) => {
     const hashedPassword = await bcrypt.hash(req.body.password, salt);
 
     const token = await new Token({
-        
-    })
+        userId: user._id,
+        token: crypto.randomBytes(32).toString("hex")
+    }).save();
+
+    const url = `http://localhost:300/users/${user._id}/verify/${token.token}`;
+
+    res.sratus(201).send({message: "EMAIL SENT TO YOUR ACCOUNT, PLEASE VERIFY TO REGISTER!"});
 
     const user = new User({
         firstname: req.body.firstname,
@@ -48,7 +53,7 @@ const handleSignin = async (req, res) => {
     if (!validPassword) {
         return res.status(404).send("INVALID PASSWORD!!!")
     }
-    // const token = jwt.sign({ _id: user._id }, process.env.TOKEN_SECRET);
+    // const token = jwt.sign({ _id: user._id }, poiuytrewqmnbvcxz);
     // res.header("auth-token", token).send(token);
     return res.send({ message: "LOGIN SUCCESSFUL!" })
 };
