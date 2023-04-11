9024fa61d101ea080d9585688b432292bb6f3b34
VALIANTGUARDIAN <singhdigvijay703@gmail.com>
2023-04-11 13:22:40 +0530

    forget controller

---
+++ controllers/resetController.js
@@ -0,0 +1,39 @@
+const User = require("../models/userModel");
+const Token = require("../models/tokenModel");
+const { sendMail } = require("../middleware/sendEmail");
+const bcrypt = require("bcrypt");
+const crypto = require("crypto");
+const jwt = require("jsonwebtoken");
+const { loginValidation } = require("../middleware/validation");
+
+const forgetPassword = async (req, res) => {
+    try {
+        const { error } = loginValidation(req, res);
+        if (error) {
+            return res.status(400).send(error.details[0].message);
+        };
+
+        const emailExist = await User.findOne({ email: req.body.email });
+        if(!emailExist){
+            return res.status(409).send({ message: "EMAIL DON'T EXISTS!!!" });
+        }
+        const resetToken = await new Token({
+            userId: userInfo._id,
+            token: cryto.randomBytes(32).toString("hex"),
+        }).save();
+        const url = `http://localhost:8080/users/${user._id}/verify/${resetToken.token}`;
+        await sendMail(req.body.email, "RESET LINK", url);
+        res.status(201).send({message:"RESET LINK SENT"});
+    } catch (error) { 
+        console.log(error);
+        return res.status(500).send({message:"EMAIL NOT SENT"});
+    }
+};
+
+const changePassword = async (req, res) =>{
+    const {id, token} = req.params;
+     const resetToken = await Token.findOne({userId: id, token});
+     if(!resetToken){
+        return res.status(400).send({error:"INVALID LINK"});
+     }
+}
