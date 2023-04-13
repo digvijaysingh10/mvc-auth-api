ac6d7fb3e7115bbfa6d260b77640db6925bc90f6
VALIANTGUARDIAN <singhdigvijay703@gmail.com>
2023-04-13 16:53:59 +0530

    .

--- server.js
+++ server.js
@@ -17,14 +17,14 @@ app.use(cors(
 
 app.use(express.json());
 
+app.use("/users", userRouter, authRouter, forgetRouter);
+
 connectMongoDB("mongodb://localhost:27017/crud-test").then(() => {
   console.log("DB Connected!!!");
 }).catch((err) => {
   console.log(`DB Connection Error: ${err.message}`);
   process.exit(1);
 });
-
-app.use("/users", userRouter, authRouter, forgetRouter);
 
 app.listen(PORT, () => {
   console.log(`Server started at http://localhost:${PORT}/`);
