80d94023abde77007621f5ccf25c76eaf19a3750
VALIANTGUARDIAN <singhdigvijay703@gmail.com>
2023-04-13 13:38:45 +0530

    .

--- controllers/changeController.js
+++ controllers/changeController.js
@@ -2,28 +2,22 @@ const User = require("../models/userModel");
 const bcrypt = require("bcrypt");
 const { loginValidation } = require("../middleware/validation");
 const jwt = require("jsonwebtoken");
-const authController = require("../controllers/authController")
 
 
 const changePassword = async (req, res) => {
   try {
-
-    const token =
-    
-    console.log(token)
+    const token = req.header("auth-token");
     if (!token) {
       return res.status(401).send({ message: "ACCESS DENIED! AUTHENTICATION TOKEN REQUIRED!" });
     }
 
     const decoded = jwt.verify(token, "poiuytrewqmnbvcxz");
     const userId = decoded._id;
- 
 
-    const user = await User.findById({_id:userId});
+    const user = await User.findById(userId);
     if (!user) {
       return res.status(404).send({ message: "USER NOT FOUND!" });
     }
-
 
     if (!user.verified) {
       return res.status(400).send({ message: "USER NOT VERIFIED! PLEASE VERIFY YOUR ACCOUNT FIRST!" });
@@ -37,23 +31,19 @@ const changePassword = async (req, res) => {
     const oldPasswordMatched = await bcrypt.compare(req.body.oldPassword, user.password);
     if (!oldPasswordMatched) {
       return res.status(401).send({ message: "OLD PASSWORD DIDN'T MATCHED!" });
-    }else{
-      const newpass=  await bcrypt.hashSync(req.body.password,10);
-     const data=await User.findOneAndUpdate({_id:userId},{$set:{password:newpass}})
+    } else {
+      const newpass = await bcrypt.hashSync(req.body.password, 10);
+      const data = await User.findOneAndUpdate({ _id: userId }, { $set: { password: newpass } })
       res.send(data)
-      // const salt = await bcrypt.genSalt(10);
-      // const hashedPassword = await bcrypt.hash(req.body.newPassword, salt);
-  
-      // await User.updateOne({ _id: userId }, { password: hashedPassword });
-  
-      // res.status(200).send({ message: "PASSWORD CHANGED SUCCESSFULLY!" });
     }
 
-   
   } catch (error) {
     console.log(error);
     return res.status(500).send({ message: "SERVER ERROR!" });
   }
 };
+
+module.exports = changePassword;
+
 
 module.exports = changePassword;
